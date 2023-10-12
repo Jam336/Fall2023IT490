c6f749a79061f21ff3a9c138397d5d5824124040
ek275 <ek275@IT490F.myguest.virtualbox.org>
2023-10-12 11:22:44 -0400

    editing front end files

--- sample/login.php
+++ sample/login.php
@@ -26,39 +26,9 @@ if (!$error)
 {
 	login($request);
 	$response = "Successfully sent to client";
+	die(header("Location: home.html"));
 }
 echo json_encode($response);
 exit(0);
 
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Login Page</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-  <div class="header">
-  	<h2>Login</h2>
-  </div>
-	 
-  <form method="post" action="login.php">
-  	<?php include('errors.php'); ?>
-  	<div class="input-group">
-  		<label>Username</label>
-  		<input type="text" name="username" >
-  	</div>
-  	<div class="input-group">
-  		<label>Password</label>
-  		<input type="password" name="password">
-  	</div>
-  	<div class="input-group">
-  		<button type="submit" class="btn" name="login_user">Login</button>
-  	</div>
-  	<p>
-  		Not yet a member? <a href="register.php">Sign up</a>
-  	</p>
-  </form>
-</body>
-</html>
